249ab06fcd54c09618be60e351042831a7588afb
Saikat-98 <saikatchoudhury1998@gmail.com>
2021-01-01 00:40:39 +0530

    Initial commit

---
+++ client/build/precache-manifest.5e75229ed48e0a43f0bc7fb03dd2c80e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29dc6559083c7be5bc491972733a0ff7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94c6d7669b761846c3d1",
+    "url": "/static/css/main.cff86ad9.chunk.css"
+  },
+  {
+    "revision": "a635fe386eba0a41d0c4",
+    "url": "/static/js/2.68606711.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.68606711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c6d7669b761846c3d1",
+    "url": "/static/js/main.510e82c9.chunk.js"
+  },
+  {
+    "revision": "1e1d4ce167f8f448348c",
+    "url": "/static/js/runtime-main.e71eddfe.js"
+  }
+]);
